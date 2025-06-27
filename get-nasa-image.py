8432506919d8fc8c5dc3bb4bd76892f1245d2a0d
Phil Brown <p_brown_100@yahoo.co.uk>
2025-06-27 14:34:58 +0100

    Create get-nasa-image.py

---
+++ get-nasa-image.py
@@ -0,0 +1,15 @@
+image_base_url=https://api.nasa.gov/planetary/apod?api_key
+
+def get_image():
+  try:
+        response = requests.get(f"{image_base_url}={os.environ.NASA_SECRET}" )
+        if response.status_code == 200:
+            hdurl = response.json().get("hdurl")
+            print(f"hdurl: {hdurl}")
+        else:
+            print("Failed to get json")
+    except Exception as e:
+        print(str(e))
+
+get_image()
+  
